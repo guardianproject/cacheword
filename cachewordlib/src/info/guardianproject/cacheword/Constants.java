7966937d410f23a17adf797f59a35323edf665f8
uberspot <onexemailx@gmail.com>
2013-10-30 14:54:10 +0100

    made vibration after unlocking cacheword optional
    default is true meaning it will vibrate
    
    fixes #2004

--- cachewordlib/src/info/guardianproject/cacheword/Constants.java
+++ cachewordlib/src/info/guardianproject/cacheword/Constants.java
@@ -18,6 +18,7 @@ public class Constants {
     public static final String SHARED_PREFS_FOREGROUND = "foreground";
     public static final String SHARED_PREFS_TIMEOUT = "cacheword_timeout";
     public static final String SHARED_PREFS_USE_NOTIFICATION = "enable_notification";
+    public static final String SHARED_PREFS_VIBRATE = "cacheword_vibrate";
 
     public static final int SERVICE_FOREGROUND_ID = 81231;
     public static final int SERVICE_BACKGROUND_ID = 13218;
