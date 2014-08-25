5f77a678b6b9bacb22086c615c23de689d27c238
Hans-Christoph Steiner <hans@eds.org>
2014-08-25 15:31:16 -0400

    include static encodeHex() method to purge commons-codec.jar
    cacheword needs to have as few deps as possible, so purge this jar.  Thanks
    to sqlcipher:
    
    https://github.com/sqlcipher/android-database-sqlcipher/commit/817cff171c99bf6dfc654df3658dbb8af0d59e25
    
    fixes #3663 https://dev.guardianproject.info/issues/3663

--- cachewordlib/src/info/guardianproject/cacheword/SQLCipherOpenHelper.java
+++ cachewordlib/src/info/guardianproject/cacheword/SQLCipherOpenHelper.java
@@ -9,8 +9,6 @@ import net.sqlcipher.database.SQLiteDatabase.CursorFactory;
 import net.sqlcipher.database.SQLiteException;
 import net.sqlcipher.database.SQLiteOpenHelper;
 
-import org.apache.commons.codec.binary.Hex;
-
 import java.lang.reflect.Method;
 import java.nio.CharBuffer;
 
@@ -108,7 +106,7 @@ public abstract class SQLCipherOpenHelper extends SQLiteOpenHelper {
             kPrefix = "x''";
             kSuffix = "''";
         }
-        final char[] key_chars = Hex.encodeHex(raw_key);
+        final char[] key_chars = encodeHex(raw_key, HEX_DIGITS_LOWER);
         if (key_chars.length != 64)
             throw new IllegalStateException("encoded key is not 64 bytes wide");
 
@@ -145,5 +143,20 @@ public abstract class SQLCipherOpenHelper extends SQLiteOpenHelper {
                 return true;
         }
         return false;
+    }
+
+    private static final char[] HEX_DIGITS_LOWER = {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+
+    private static char[] encodeHex(final byte[] data, final char[] toDigits) {
+        final int l = data.length;
+        final char[] out = new char[l << 1];
+        // two characters form the hex value.
+        for (int i = 0, j = 0; i < l; i++) {
+            out[j++] = toDigits[(0xF0 & data[i]) >>> 4];
+            out[j++] = toDigits[0x0F & data[i]];
+        }
+        return out;
     }
 }
